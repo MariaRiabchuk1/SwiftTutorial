d75fdae6a4f1a08b3396e9b5b10df583fe081a59
Maria <rjdnansd@gmail.com>
2022-05-26 22:47:04 +0300

    add tableview

--- Tutorial/ViewController.swift
+++ Tutorial/ViewController.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("hello")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+}
+
 
